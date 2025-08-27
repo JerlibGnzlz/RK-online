a67951134f249fb983315566505e32e0e93ce02c
jerlibgnzlz <jerlibgnzlz@gmail.com>
2025-08-27 00:18:38 -0300

    instalacion de RQ6

--- react-query-store/src/products/pages/ProductById.tsx
+++ react-query-store/src/products/pages/ProductById.tsx
@@ -1,11 +1,16 @@
 import { useParams } from "react-router-dom"
 import { useProductId } from "../hooks/useProductId"
 import { ProductCard } from "../components/ProductCard"
+import { useEffect } from "react"
 
 
 
 // export const CompleteListPage = (): Products => {
 export const ProductById = () => {
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
 
     const { id } = useParams()
 
