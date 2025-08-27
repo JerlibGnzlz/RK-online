09e728a560af379179e8119ce158264d7e1dea70
jerlibgnzlz <jerlibgnzlz@gmail.com>
2025-08-26 23:07:55 -0300

    instalacion de RQ4

--- react-query-store/src/products/services/actions.ts
+++ react-query-store/src/products/services/actions.ts
@@ -5,14 +5,18 @@ import { type Products } from "../interfaces/productos";
 
 interface GetProductsOptions {
     filterKey?: string
-
 }
 
 
+const sleep = (seconds: number) => new Promise(resolve => setTimeout(resolve, seconds * 1000));
+
 export const getProducts = async ({ filterKey }: GetProductsOptions) => {
 
+    await sleep(2);
 
-    const { data } = await productApi.get<Products[]>('/products');
+    const filteredUrl = filterKey ? `category=${filterKey}` : '';
+
+    const { data } = await productApi.get<Products[]>(filteredUrl ? `/products?${filteredUrl}` : '/products');
 
     return data;
 };
